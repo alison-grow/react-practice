395a8851d6e89e1af901a7f599cc5cbf1eeacc3f
alison-grow <agrowz98@gmail.com>
2022-08-09 21:05:40 -0700

    little notes

---
+++ day1/day1.js
@@ -0,0 +1,35 @@
+// do not use var to assign variables - antiquated
+
+// use const(best to use) or let(not best but ok)
+
+
+
+
+
+
+
+//space between curly brackets is called a block
+
+// ex: function() {
+
+    // this area is called a block
+// }
+
+// if you assign let or const inside a block it will only execute code within that block
+
+// good example: function() {
+    // let x = 5
+    // console.log(x)
+// }
+
+// bad example: function() {
+    // let x = 5
+// }
+
+// console.log(x) -- THIS WILL NOT WORK BECAUSE IT IS OUTSIDE OF BLOCK
+
+
+
+
+
+
